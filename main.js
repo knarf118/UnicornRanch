54b1b52652363d72f64597f8123f1a90584839e5
Frank Holder <luckystarboy@gmail.com>
2019-04-29 21:50:17 -0400

    use firebase database

--- main.js
+++ main.js
@@ -6,17 +6,40 @@ var config = {
   storageBucket: "unicornranch-ade06.appspot.com",
   messagingSenderId: "832118612204"
 };
+firebase.initializeApp(config);
+
+let getLocation = location => {
+  const ref = firebase.database().ref(location.id);
+  ref.on(
+    "value",
+    snapshot => {
+      if (snapshot.val()) {
+        location.value = snapshot.val().value;
+      }
+    },
+    error => {
+      console.error("The read failed: " + error.code);
+    }
+  );
+};
 
 let updateInfo = x => {
   let newLocation = x.value;
-  localStorage[x.id] = newLocation;
+  firebase
+    .database()
+    .ref(x.id)
+    .set({
+      value: newLocation
+    })
+    .catch(error => {
+      alert("Data could not be saved." + error);
+      console.error(error);
+    });
 };
 
 let showLocation = () => {
   let x = document.getElementsByTagName("select");
   for (let index = 0; index < x.length; index++) {
-    if (localStorage[x[index].id]) {
-      x[index].value = localStorage[x[index].id];
-    }
+    getLocation(x[index]);
   }
 };
